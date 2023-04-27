2744e383d176201222e9ed7db53b64a2987db832
yeebay <jeeyeepay@gmail.com>
2023-04-28 01:41:06 +0800

    Change 为 store 添加 loadingReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loadingReducer from '../reducers/loading';
 
 export default configureStore({
-	reducer: {},
+	reducer: {
+		loading: loadingReducer,
+	},
 });
