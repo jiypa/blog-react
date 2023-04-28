2160d77e93f82c3b261880cbbd3febb83da406ea
yeebay <jeeyeepay@gmail.com>
2023-04-28 22:51:08 +0800

    Change 封装根组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import './App.css';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import HomeView from './views/HomeView';
+import NavBar from './components/NavBar';
+import FootBar from './components/FootBar';
+import BackTop from './components/BackTop';
+import LoadingBackdrop from './components/LoadingBackdrop';
 
+// 根组件
 export default function App() {
+	const { pathname } = useLocation();
+	const isRootPath = pathname === '/';
+	const isNotFoundPath = pathname === '/404';
+	const isUserPath = pathname.startsWith('/user');
+
 	return (
-		<div>{'App'}</div>
+		<>
+			<SnackbarProvider maxSnack={3} autoHideDuration={2000}>
+				<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+					{!isNotFoundPath && !isUserPath ? <NavBar pathname={pathname}/> : null}
+					{isRootPath ? <HomeView/> : <Outlet/>}
+					{!isNotFoundPath && !isUserPath ? <FootBar/> : null}
+					<BackTop/>
+				</Box>
+			</SnackbarProvider>
+			<LoadingBackdrop/>
+		</>
 	);
 }
