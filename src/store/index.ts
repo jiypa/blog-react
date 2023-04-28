68b05db78f8593d05c7cae3831ae65e2b0f3bcb1
yeebay <jeeyeepay@gmail.com>
2023-04-28 13:26:11 +0800

    Change 将 loadingReducer 文件夹重新改为 loading 文件夹

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loadingReducer from '../reducers/loadingReducer';
+import loadingReducer from '../reducers/loading';
 
 export default configureStore({
 	reducer: {
