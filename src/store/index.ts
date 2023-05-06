eaa92592596927c0858b34aa5acd12f4b68c73fc
yeebay <jeeyeepay@gmail.com>
2023-05-07 02:59:26 +0800

    Change 为 store 添加 tokenReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loadingReducer from '../reducers/loading';
+import tokenReducer from '../reducers/token';
 
 export default configureStore({
 	reducer: {
 		loading: loadingReducer,
+		token: tokenReducer,
 	},
 });
