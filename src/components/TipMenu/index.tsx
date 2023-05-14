f5d588b6c13efce140aa87be6b12ac533c43ec73
yeebay <jeeyeepay@gmail.com>
2023-05-14 18:06:58 +0800

    Change 封装提示菜单组件

---
+++ src/components/TipMenu/index.tsx
@@ -0,0 +1,41 @@
+// 提示菜单组件
+import React, { useState } from 'react';
+import {
+	IconButton,
+	Menu,
+	MenuItem,
+} from '@mui/material';
+
+interface Props {
+	icon: React.ReactElement;
+	options: string[];
+	handlers: (() => void)[];
+}
+
+export default function TipMenu(props: Props) {
+	const { icon, options = [], handlers = [] } = props;
+	const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+	const open = Boolean(anchorEl);
+
+	return (
+		<>
+			<IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
+				{icon}
+			</IconButton>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={() => setAnchorEl(null)}
+			>
+				{options?.map((option, index) => (
+					<MenuItem key={option} onClick={() => {
+						handlers?.[index]?.();
+						setAnchorEl(null);
+					}}>
+						{option}
+					</MenuItem>
+				))}
+			</Menu>
+		</>
+	);
+}
