37386a96b8337f1b4cc7a7ece84021bb7401a399
jiypa <3394521327@qq.com>
2024-06-05 13:26:03 +0800

    feat: 优化全局底部栏组件

--- src/components/FootBar/index.tsx
+++ src/components/FootBar/index.tsx
@@ -1,8 +1,12 @@
 // 全局底部栏组件
 import React from 'react';
-import { Box } from '@mui/material';
+import dayjs from 'dayjs';
 import styles from './index.module.less';
 
 export default function FootBar() {
-	return <Box className={styles.container}>{'Copyright © 2021-2023 YEEBAY\'S BLOG'}</Box>;
+	return <footer className={styles.container}>
+		<span>{`本站已稳定运行 ${dayjs().diff('2022-04-26', 'day')} 天`}</span>
+		<span>{'Copyright © 2022-2024 寄依的博客'}</span>
+		<span>{'All Rights Reserved.'}</span>
+	</footer>;
 }
