a8f86e0fb9c097621cd9573f4f82cf15a8bf7686
jiypa <3394521327@qq.com>
2024-06-05 17:26:17 +0800

    feat: 优化用户后台底部栏组件

--- src/components/UserFootBar/index.tsx
+++ src/components/UserFootBar/index.tsx
@@ -1,8 +1,7 @@
 // 用户后台底部栏组件
 import React from 'react';
-import { Box } from '@mui/material';
 import styles from './index.module.less';
 
 export default function UserFootBar() {
-	return <Box className={styles.container}>{'博客后台系统'}</Box>;
+	return <footer className={styles.container}>{'博客后台系统'}</footer>;
 }
