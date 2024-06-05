2338e1ce27a4f1e9275e5a0090423e380aa48d5e
jiypa <3394521327@qq.com>
2024-06-05 19:56:42 +0800

    feat: 优化分割线组件

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,15 +1,14 @@
 // 封装一个分割线组件以解决 MUI 的 Divider 组件失效问题
 import React from 'react';
+import { Property } from 'csstype';
 import { Box } from '@mui/material';
-import usePalette from '../../hooks/usePalette';
 
 interface Props {
-    color?: string;
+    color?: Property.Color | undefined;
 }
 
 export default function Separator(props: Props) {
-	const { palette } = usePalette();
-	const { color = palette.c_main_white } = props;
+	const { color } = props;
 
-	return <Box style={{ width: '100%', height: 1, backgroundColor: color }}/>;
+	return <Box style={{ height: 1, backgroundColor: color ?? 'var(--gray-main-color)' }}/>;
 }
