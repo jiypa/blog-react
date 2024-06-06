7d466496721bf4afe4b57cb01e76a2ee74ce27e2
jiypa <3394521327@qq.com>
2024-06-06 23:22:32 +0800

    fix: 修复分割线不能正常显示问题

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,7 +1,6 @@
 // 封装一个分割线组件以解决 MUI 的 Divider 组件失效问题
 import React from 'react';
 import { Property } from 'csstype';
-import { Box } from '@mui/material';
 
 interface Props {
     color?: Property.Color | undefined;
@@ -10,5 +9,9 @@ interface Props {
 export default function Separator(props: Props) {
 	const { color } = props;
 
-	return <Box style={{ height: 1, backgroundColor: color ?? 'var(--gray-main-color)' }}/>;
+	return <div style={{
+		width: '100%',
+		height: 1,
+		backgroundColor: color || 'var(--gray-main-color)',
+	}}/>;
 }
