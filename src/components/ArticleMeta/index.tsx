050a7e7c6a5b2f3a973ad083ff6c3a9f5f582c9f
jiypa <3394521327@qq.com>
2024-06-08 16:35:09 +0800

    feat: 优化文章元数据组件

--- src/components/ArticleMeta/index.tsx
+++ src/components/ArticleMeta/index.tsx
@@ -1,35 +1,35 @@
 // 文章元数据组件
 import React from 'react';
-import { Stack } from '@mui/material';
-import {
-	AccessTimeOutlined,
-	FolderOutlined,
-	ClassOutlined,
-	VisibilityOutlined,
-} from '@mui/icons-material';
 import MetaIcon from '../MetaIcon';
 
 interface Meta {
 	updatedTime?: string;
 	category?: string;
 	tags?: string;
-	views?: number;
+	views?: string;
 }
 interface Props {
-	size: number;
 	meta: Meta;
 }
 
+// 竖线
+function VSeparator() {
+	return <div style={{ width: 1, height: '1rem', backgroundColor: 'var(--gray-a-color)' }}/>;
+}
+
 export default function ArticleMeta(props: Props) {
-	const { size, meta } = props;
+	const { meta } = props;
 	const { updatedTime, category, tags, views } = meta;
 
 	return (
-		<Stack style={{ marginBottom: 20 }} direction={'row'} spacing={2}>
-			<MetaIcon icon={<AccessTimeOutlined sx={{ fontSize: size }} color={'primary'}/>} desc={updatedTime ?? ''}/>
-			<MetaIcon icon={<FolderOutlined sx={{ fontSize: size }} color={'primary'}/>} desc={category ?? ''}/>
-			<MetaIcon icon={<ClassOutlined sx={{ fontSize: size }} color={'primary'}/>} desc={tags ?? ''}/>
-			<MetaIcon icon={<VisibilityOutlined sx={{ fontSize: size }} color={'primary'}/>} desc={`浏览${views ?? ''}次`}/>
-		</Stack>
+		<div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+			<MetaIcon hint={'更新时间'} icon={'ic:round-access-time'} desc={updatedTime ?? ''}/>
+			<VSeparator/>
+			<MetaIcon hint={'文章分类'} icon={'ic:round-folder-open'} desc={category ?? ''}/>
+			<VSeparator/>
+			<MetaIcon hint={'文章标签'} icon={'ic:round-tag'} desc={tags ?? ''}/>
+			<VSeparator/>
+			<MetaIcon hint={'浏览次数'} icon={'ic:round-visibility'} desc={views ?? ''}/>
+		</div>
 	);
 }
