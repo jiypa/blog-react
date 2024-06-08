462d97272008c5e14e1a33bb41e5ebe8fbbda9f1
jiypa <3394521327@qq.com>
2024-06-08 15:27:53 +0800

    feat: 优化搜索栏组件

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,30 +1,32 @@
 // 搜索栏组件
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, CSSProperties } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useKeyPress } from 'ahooks';
-import { Box, IconButton } from '@mui/material';
-import { Search } from '@mui/icons-material';
+import { Icon } from '@iconify/react';
 import useToast from '../../hooks/useToast';
-import usePalette from '../../hooks/usePalette';
 import styles from './index.module.less';
 
 interface SearchBarProps {
+	style?: CSSProperties;
 	active?: boolean;
     placeholder?: string;
+	onClick?: () => void;
 }
 
 export default function SearchBar(props: SearchBarProps) {
-	const { active, placeholder } = props;
+	const { style, active, placeholder, onClick } = props;
 	const [value, setValue] = useState<string>('');
 	// 解决 TS 下 inputRef.current.value 报错问题
 	const inputRef = useRef<HTMLInputElement>(null);
 	const navigate = useNavigate();
 	const { toast } = useToast();
-	const { palette } = usePalette();
 
 	useKeyPress(
 		'enter',
-		() => searchArticle(),
+		() => {
+			onClick?.();
+			searchArticle();
+		},
 		{
 			// 键盘按下时不触发，弹起时才触发
 			events: ['keyup'],
@@ -43,18 +45,7 @@ export default function SearchBar(props: SearchBarProps) {
 	}
 
 	return (
-		<Box className={active ? styles.containerActive : styles.container}>
-			<IconButton
-				style={{
-					width: 24,
-					height: 24,
-					marginLeft: 2,
-					color: active ? palette.c_font_black : palette.c_main_white,
-				}}
-				onClick={() => searchArticle()}
-			>
-				<Search sx={{ fontSize: 16 }}/>
-			</IconButton>
+		<div className={active ? styles.containerActive : styles.container} style={style}>
 			<input
 				ref={inputRef}
 				className={active ? styles.inputActive : styles.input}
@@ -62,6 +53,18 @@ export default function SearchBar(props: SearchBarProps) {
 				value={value}
 				onChange={(event) => setValue(event.target.value)}
 			/>
-		</Box>
+			<div className={styles.buttonContainer}>
+				<Icon
+					icon='ic:round-search'
+					width='1rem'
+					height='1rem'
+					style={!active ? { color: 'var(--white-main-color)' } : {}}
+					onClick={() => {
+						onClick?.();
+						searchArticle();
+					}}
+				/>
+			</div>
+		</div>
 	);
 }
