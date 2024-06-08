0ad9b5c3414c29a99223d2f2a411da80cefe6e1b
jiypa <3394521327@qq.com>
2024-06-08 16:24:36 +0800

    feat: 优化文章元数据项组件

--- src/components/MetaIcon/index.tsx
+++ src/components/MetaIcon/index.tsx
@@ -1,21 +1,27 @@
 // 文章元数据项组件
-import React, { ReactElement } from 'react';
+import React from 'react';
+import { Tooltip } from '@mui/material';
+import { Icon } from '@iconify/react';
 
 interface Props {
-    icon: ReactElement;
-    desc: string;
+	hint: string;
+	icon: string;
+	desc: string;
 }
 
 export default function MetaIcon(props: Props) {
-	const { icon, desc } = props;
+	const { hint, icon, desc } = props;
 
 	return (
-		<div style={{
-			display: 'flex',
-			alignItems: 'center',
-			gap: 2,
-		}}>
-			{icon}
+		<div style={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+			<Tooltip arrow title={hint}>
+				<Icon
+					icon={icon}
+					width='1rem'
+					height='1rem'
+					style={{ color: 'var(--blue-main-color)' }}
+				/>
+			</Tooltip>
 			<span>{desc}</span>
 		</div>
 	);
