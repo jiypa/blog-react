966197f08f7a314022a1d3c180e76c8ae0288499
jiypa <3394521327@qq.com>
2024-06-13 03:20:57 +0800

    add: 添加全局顶部导航栏配置文件

---
+++ src/config/nav.config.ts
@@ -0,0 +1,32 @@
+const NavMap = new Map([
+	[
+		'/',
+		{
+			title: '首页',
+			icon: 'ic:round-home',
+		}
+	],
+	[
+		'/archive',
+		{
+			title: '归档',
+			icon: 'ic:round-archive',
+		}
+	],
+	[
+		'/tool',
+		{
+			title: '工具',
+			icon: 'ic:round-handyman',
+		}
+	],
+	[
+		'/about',
+		{
+			title: '关于',
+			icon: 'ic:round-info',
+		}
+	],
+]);
+
+export default NavMap;
