520e22e19fb8c6e1dac7d8ec41074fc9d017f22a
jiypa <3394521327@qq.com>
2024-06-14 05:13:48 +0800

    feat: 优化「首页」页面

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,25 +1,22 @@
 import React, {
 	useState,
 	useEffect,
+	useRef,
 	useMemo,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTitle } from 'ahooks';
-import {
-	Box,
-	Avatar,
-	Pagination,
-	Stack,
-} from '@mui/material';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
-import { Dayjs } from 'dayjs';
+import { Pagination } from '@mui/material';
+import { Icon } from '@iconify/react';
+import Typed from 'typed.js';
 import useRequest from '../../hooks/useRequest';
 import useScrollTop from '../../hooks/useScrollTop';
-import usePalette from '../../hooks/usePalette';
+import useUrl from '../../hooks/useUrl';
+import Banner from '../../components/Banner';
 import Empty from '../../components/Empty';
 import ArticleCard from '../../components/ArticleCard';
 import Separator from '../../components/Separator';
+import { ArticleCategories } from '../../components/ArticleMetaDialog';
 import styles from './index.module.less';
 import AvatarImg from '../../assets/images/img_avatar.png';
 
@@ -45,19 +42,31 @@ interface PageData {
 export default function HomeView() {
 	const [pageData, setPageData] = useState<PageData | null>(null);
 	const [pageIndex, setPageIndex] = useState<number>(1);
-	const [date, setDate] = useState<Dayjs | null>(null);
+	const mottoRef = useRef<HTMLSpanElement>(null);
 	const articles = useMemo<Article[]>(() => pageData?.articles ?? [], [pageData]);
 	const navigate = useNavigate();
 	const { request } = useRequest();
 	const { scrollTop } = useScrollTop();
-	const { palette } = usePalette();
+	const { openUrl } = useUrl();
 
 	useTitle('首页');
+	useEffect(() => {
+		const typed = new Typed(mottoRef.current, {
+			strings: ['永远做脚踏实地的追梦人', '不要做泛泛而谈的空想家'],
+			typeSpeed: 150,
+			loop: true,
+			showCursor: false,
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+
 	useEffect(() => {
 		request.get('/home', {
 			params: {
 				page: pageIndex,
-				date: date?.format('YYYY-MM-DD'),
 			}
 		})
 			.then(({ data }) => {
@@ -66,65 +75,128 @@ export default function HomeView() {
 			.catch((err) => {
 				console.log('err', err);
 			});
-	}, [pageIndex, date]);
+	}, [pageIndex]);
 
 	return (
-		<Stack sx={{ justifyContent: 'center' }} direction={'row'} spacing={2}>
-			<Box className={styles.leftContainer}>
-				{
-					articles?.length ? articles?.map(({ pid, title, content }) => <Box
-						key={pid}
-						onClick={() => navigate(`/article/p/${pid}`)}
-					>
-						<ArticleCard title={title} content={content}/>
-						<Separator color={palette.c_main_gray}/>
-					</Box>) : date ? <Empty title={'暂无数据'}/> : null
-				}
-				<Box className={styles.paginationContainer}>
-					<Pagination
-						count={pageData?.totalPages}
-						color={'primary'}
-						shape={'rounded'}
-						variant={'outlined'}
-						onChange={(event, page) => {
-							setPageIndex(page);
-							scrollTop('smooth');
-						}}
-					/>
-				</Box>
-			</Box>
-			<Box>
-				<Box className={styles.rightItem}>
-					<Avatar
-						style={{ width: 70, height: 70, marginBottom: 20 }}
-						alt={'头像'}
-						src={AvatarImg}
-					/>
-					<h4 style={{ marginBottom: 20 }}>{'yeebay'}</h4>
-					<p style={{ marginBottom: 10 }}>{'永远做脚踏实地的追梦人'}</p>
-					<p>{'不要做泛泛而谈的空想家'}</p>
-				</Box>
-				<Box className={styles.rightItem} style={{ padding: 0 }}>
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DateCalendar value={date} onChange={(date) => setDate(date)}/>
-					</LocalizationProvider>
-				</Box>
-				<Box className={styles.rightItem}>
-					<h4 style={{ marginBottom: 20, letterSpacing: 1, color: palette.c_font_black }}>{'近期更新的文章'}</h4>
-					<Separator/>
-					<Box sx={{ width: '100%' }}>
-						{
-							pageData?.top5Articles?.map(({ pid, title }, index) => <p
-								style={{ marginTop: index === 0 ? 20 : 10, marginLeft: 20, marginRight: 20 }}
-								onClick={() => navigate(`/article/p/${pid}`)}
-								key={pid}
-							>
-								{`${index + 1}. ${title}`}
-							</p>)
-						}
-					</Box>
-				</Box>
-			</Box>
-		</Stack>
+		<>
+			<Banner
+				height={'100vh'}
+				title={'寄依'}
+				customContent={<span ref={mottoRef} className={styles.motto}/>}
+			/>
+			<main className={styles.container}>
+				<section className={styles.leftContainer}>
+					{
+						articles?.length ? articles?.map(({ pid, title, content }) => <ArticleCard
+							key={pid}
+							title={title}
+							content={content}
+							onClick={() => navigate(`/article/p/${pid}`)}
+						/>) : <Empty title={'暂无数据'}/>
+					}
+					<div style={{ display: 'flex', justifyContent: 'center' }}>
+						<Pagination
+							count={pageData?.totalPages}
+							color={'primary'}
+							shape={'rounded'}
+							variant={'outlined'}
+							onChange={(event, page) => {
+								setPageIndex(page);
+								scrollTop('smooth');
+							}}
+						/>
+					</div>
+				</section>
+				<section className={styles.rightContainer}>
+					<section className={styles.rightCard}>
+						<img
+							src={AvatarImg}
+							alt={'寄依'}
+							style={{ width: '3rem', height: '3rem' }}
+						/>
+						<span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{'寄依'}</span>
+						<span>{'永远做脚踏实地的追梦人'}</span>
+						<span>{'不要做泛泛而谈的空想家'}</span>
+						<div style={{ display: 'flex', gap: '.75rem' }}>
+							<Icon
+								icon={'ant-design:github-filled'}
+								width={'1.5rem'}
+								height={'1.5rem'}
+								style={{ color: '#333' }}
+								onClick={() => openUrl('https://github.com/jiypa', 'blank')}
+							/>
+							<Icon
+								icon={'ant-design:mail-filled'}
+								width={'1.5rem'}
+								height={'1.5rem'}
+								style={{ color: '#feb42c' }}
+								onClick={() => openUrl('mailto:3394521327@qq.com', 'blank')}
+							/>
+							<Icon
+								icon={'ant-design:qq-circle-filled'}
+								width={'1.5rem'}
+								height={'1.5rem'}
+								style={{ color: '#09f' }}
+								onClick={() => openUrl('https://qm.qq.com/q/uU9p3TBASQ', 'blank')}
+							/>
+						</div>
+					</section>
+					<section className={styles.rightCard}>
+						<header className={styles.rightCardTitle}>{'最新文章'}</header>
+						<Separator/>
+						<div className={styles.latestArticles}>
+							{
+								pageData?.top5Articles?.map(({ pid, title }, index) => <span
+									key={pid}
+									onClick={() => navigate(`/article/p/${pid}`)}
+								>
+									{`${index + 1}. ${title}`}
+								</span>)
+							}
+						</div>
+					</section>
+					<section className={styles.rightCard}>
+						<header className={styles.rightCardTitle}>{'文章分类'}</header>
+						<Separator/>
+						<div className={styles.articleCategories}>
+							{
+								ArticleCategories?.map((category, index) => <div
+									key={index}
+									className={styles.rightCardItem}
+								>
+									<Icon
+										icon={'ic:round-folder-open'}
+										width={'1rem'}
+										height={'1rem'}
+										style={{ color: 'inherit' }}
+									/>
+									{`${category}`}
+								</div>)
+							}
+						</div>
+					</section>
+					<section className={styles.rightCard}>
+						<header className={styles.rightCardTitle}>{'文章标签'}</header>
+						<Separator/>
+						<div className={styles.articleTags}>
+							{
+								ArticleCategories?.map((category, index) => <div
+									key={index}
+									className={styles.rightCardItem}
+								>
+									<Icon
+										icon={'ic:round-tag'}
+										width={'1rem'}
+										height={'1rem'}
+										style={{ color: 'inherit' }}
+									/>
+									{`${category}`}
+								</div>)
+							}
+						</div>
+					</section>
+				</section>
+			</main>
+		</>
 	);
 }
