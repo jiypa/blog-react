bfcc61b659b455b02a298f70f86cbb6af81d25dd
jiypa <3394521327@qq.com>
2024-06-15 18:48:38 +0800

    Revert "fix: 解决移动端回到顶部组件点击过后不变色问题"
    This reverts commit 37fcd348391bb840c9b53bdb927b6efb2b32befc.

--- src/components/BackTop/index.tsx
+++ src/components/BackTop/index.tsx
@@ -1,6 +1,6 @@
 // 回到顶部组件
 // 此组件为 MUI 示例组件，相关链接：https://mui.com/material-ui/react-app-bar/#back-to-top
-import React, { useState } from 'react';
+import React from 'react';
 import {
 	Fade,
 	Fab,
@@ -8,9 +8,9 @@ import {
 } from '@mui/material';
 import { Icon } from '@iconify/react';
 import useScrollTop from '../../hooks/useScrollTop';
+import styles from './index.module.less';
 
 export default function BackTop() {
-	const [active, setActive] = useState<boolean>(false);
 	const trigger = useScrollTrigger({
 		disableHysteresis: true,
 		threshold: 100,
@@ -19,28 +19,13 @@ export default function BackTop() {
 
 	return (
 		<Fade in={trigger}>
-			<div
-				style={{
-					position: 'fixed',
-					bottom: '2rem',
-					right: '2rem',
-					zIndex: 999,
-				}}
-				onClick={() => scrollTop('smooth')}
-			>
-				<Fab
-					size={'small'}
-					onMouseOver={() => setActive(true)}
-					onMouseOut={() => setActive(false)}
-					onTouchStart={() => setActive(true)}
-					onTouchEnd={() => setActive(false)}
-					style={{ backgroundColor: active ? 'var(--blue-main-color)' : 'white' }}
-				>
+			<div className={styles.container} onClick={() => scrollTop('smooth')}>
+				<Fab className={styles.fab} size={'small'}>
 					<Icon
+						className={styles.icon}
 						icon={'ic:round-arrow-upward'}
 						width={'1.25rem'}
 						height={'1.25rem'}
-						style={active ? { color: 'white' } : {}}
 					/>
 				</Fab>
 			</div>
