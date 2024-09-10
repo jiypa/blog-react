c88b4de437635895d5a5a976a258849171f73d78
jiypa <3394521327@qq.com>
2024-09-11 05:44:44 +0800

    feat: 添加 vite-plugin-minify 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
@@ -13,6 +14,8 @@ export default defineConfig({
 		eslintPlugin({
 			include: ['src/**/*.ts', 'src/**/*.ts', 'src/*.tsx', 'src/*.tsx'],
 		}),
+		// Minify html in production
+		ViteMinifyPlugin(),
 		visualizer({
 			open: true,
 		}),
