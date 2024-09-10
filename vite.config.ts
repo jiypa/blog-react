188c37399d88266792e99877505206a9d6dcfc38
jiypa <3394521327@qq.com>
2024-09-10 23:27:01 +0800

    feat: 添加 rollup-plugin-visualizer 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,9 @@ export default defineConfig({
 		react(),
 		eslintPlugin({
 			include: ['src/**/*.ts', 'src/**/*.ts', 'src/*.tsx', 'src/*.tsx'],
+		}),
+		visualizer({
+			open: true,
 		}),
 	],
 });
